cda576a19fe3e5571b9c41679e30cea514199025
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-12-07 07:24:49 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    flowbite.content(),
+    // flowbite.content(),
  // For React
   ],
   theme: {
@@ -14,6 +14,6 @@ module.exports = {
     },
   },
   plugins: [
-    flowbite.plugin(),
+    // flowbite.plugin(),
   ],
 };
