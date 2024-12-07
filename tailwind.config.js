8f5ea81b06758f81ee94ac20a52bf57924f46419
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-12-07 10:59:36 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    // flowbite.content(),
- // For React
-  ],
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        lobster: ["Lobster", "sans-serif"],
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    // flowbite.plugin(),
-  ],
+  plugins: [],
 };
